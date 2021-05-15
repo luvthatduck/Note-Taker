bb248817c25af34452efb2a3c0d7ec47d7112bfa
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-05-14 22:29:10 -0600

    getting notes to post

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
-
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
 
 const { savedNotes } = require('./db/db.json');
 
-function createNewNote(body,savedNotesArray) {
+function createNewNote(body, savedNotesArray) {
   const notes = body;
   savedNotesArray.push(notes);
   fs.writeFileSync(
@@ -19,14 +22,35 @@ function createNewNote(body,savedNotesArray) {
   return notes;
 }
 
+// validate the data of notes
+
+function validateNotes(notes) {
+  if (!notes.title || typeof notes.title !== 'string') {
+    return false;
+  }
+  if (!notes.text || typeof notes.text !== 'string') {
+    return false;
+  }
+  return true;
+}
+
+// get routes 
+
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname,'./public/assets/notes.html'));
+  res.sendFile(path.join(__dirname, './public/assets/notes.html'));
 });
 
-app.get('*', (req, res)  => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/assets/index.html'));
 });
 
+app.get('./db/db.json', (req, res) => {
+  const results = savedNotes;
+  console.log(req.query)
+  res.json(results);
+})
+
+// must be at the bottom or it will override all get routes
 app.get('/api/notes', (req, res) => {
   return res.json(savedNotes);
 });
@@ -36,15 +60,30 @@ app.post('/api/notes', (req, res) => {
   req.body.id = savedNotes.length.toString();
 
   // if any data in req.body is incorrect, send 400 error back
-  // if (!validateAnimal(req.body)) {
-  //   res.status(400).send('Your Note is not properly formatted.');
-  // } else {
+  if (!validateNotes(req.body)) {
+    res.status(400).send('Your note is not properly formatted.');
+  } else {
     const newNote = createNewNote(req.body, savedNotes);
-    res.json(newNote);
-  // }
+    res.json(savedNotes);
+  }
 });
 
+app.delete('api/notes/:id', (req, res) => {
+  savedNotes = savedNotes((note) => {
+    if (note.id !== req.params.id) {
+      return true
+    }
+    return false
+  })
 
-app.listen(3001, () => {
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(notes,null,2)
+  )
+  res.json(notes);
+})
+
+
+app.listen(PORT, () => {
   console.log(`API server now on port 3001!`);
 });

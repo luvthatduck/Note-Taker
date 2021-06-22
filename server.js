ad24c2d65a77631b5a96dcc6ad811cb5e2ce79e5
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-06-21 22:34:55 -0600

    getting the buttons to function

--- server.js
+++ server.js
@@ -9,26 +9,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-const { savedNotes } = require('./db/db.json');
+let notes = require('./db/db.json');
 
 function createNewNote(body, savedNotesArray) {
-  const notes = body;
-  savedNotesArray.push(notes);
+  const note = body;
+  savedNotesArray.push(note);
   fs.writeFileSync(
     path.join(__dirname, './db/db.json'),
-    JSON.stringify({ savedNotes: savedNotesArray }, null, 2)
+    JSON.stringify( savedNotesArray, null, 2)
   );
 
-  return notes;
+  return note;
 }
 
 // validate the data of notes
 
-function validateNotes(notes) {
-  if (!notes.title || typeof notes.title !== 'string') {
+function validateNotes(note) {
+  if (!note.title || typeof note.title !== 'string') {
     return false;
   }
-  if (!notes.text || typeof notes.text !== 'string') {
+  if (!note.text || typeof note.text !== 'string') {
     return false;
   }
   return true;
@@ -37,48 +37,58 @@ function validateNotes(notes) {
 // get routes 
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/assets/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/assets/index.html'));
+
+app.get('/api/notes', (req, res) => {
+  return res.json(notes);
 });
 
-app.get('./db/db.json', (req, res) => {
-  const results = savedNotes;
+app.get('./db/db', (req, res) => {
+  let results = notes;
   console.log(req.query)
   res.json(results);
 })
 
-// must be at the bottom or it will override all get routes
-app.get('/api/notes', (req, res) => {
-  return res.json(savedNotes);
+
+//must be at the bottom or it will override all get routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+
+//post routes
 
 app.post('/api/notes', (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = savedNotes.length.toString();
+  req.body.id = notes.length.toString();
 
   // if any data in req.body is incorrect, send 400 error back
   if (!validateNotes(req.body)) {
     res.status(400).send('Your note is not properly formatted.');
   } else {
-    const newNote = createNewNote(req.body, savedNotes);
-    res.json(savedNotes);
+    const newNote = createNewNote(req.body, notes);
+    res.json(notes);
   }
 });
 
-app.delete('api/notes/:id', (req, res) => {
-  savedNotes = savedNotes((note) => {
-    if (note.id !== req.params.id) {
-      return true
-    }
-    return false
+
+// DELETE the note when the button is clicked
+app.delete('/api/notes/:id', (req, res) => {
+ 
+  notes = notes.filter((note) => {
+      if (note.id !== req.params.id) {
+          
+          return true;
+      }
+      
+      return false;
   })
 
   fs.writeFileSync(
-    path.join(__dirname, './db/db.json'),
-    JSON.stringify(notes,null,2)
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify(notes, null, 2)
   )
   res.json(notes);
 })
